c7513c8bef55e1522fd75d0d73d4e175ffbf276f
Tobias Reiter <t.reiter1@live.de>
2021-09-07 13:41:47 +0200

    Added pybind11 external project

--- Wrapping/Python/pyWrap.cpp
+++ Wrapping/Python/pyWrap.cpp
@@ -6,10 +6,10 @@
 // correct module name macro
 #define TOKENPASTE_INTERNAL(x, y, z) x##y##z
 #define TOKENPASTE(x, y, z) TOKENPASTE_INTERNAL(x, y, z)
-#define VIENNALS_MODULE_NAME TOKENPASTE(viennaLS, VIENNALS_PYTHON_DIMENSION, d)
+#define VIENNARAY_MODULE_NAME TOKENPASTE(viennaRay, VIENNARAY_PYTHON_DIMENSION, d)
 #define STRINGIZE2(s) #s
 #define STRINGIZE(s) STRINGIZE2(s)
-#define VIENNALS_MODULE_VERSION STRINGIZE(VIENNALS_VERSION)
+#define VIENNARAY_MODULE_VERSION STRINGIZE(VIENNARAY_VERSION)
 
 #include <pybind11/iostream.h>
 #include <pybind11/pybind11.h>
@@ -77,7 +77,7 @@ class PyrayParticle : public rayParticle<PyrayParticle, T>
 // };
 
 // module specification
-PYBIND11_MODULE(VIENNALS_MODULE_NAME, module)
+PYBIND11_MODULE(VIENNARAY_MODULE_NAME, module)
 {
     module.doc() =
         " ViennaRay is a flux calculation library for topography simulations, "
@@ -92,7 +92,7 @@ PYBIND11_MODULE(VIENNALS_MODULE_NAME, module)
     module.def("setNumThreads", &omp_set_num_threads);
 
     // rayTrace
-    
+    // pybind11::class_<rayTrace<T, D>,     
 
     // lsAdvect
     //   pybind11::class_<lsAdvect<T, D>, lsSmartPointer<lsAdvect<T, D>>>(module,
